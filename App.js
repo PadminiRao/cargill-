84a288441db760004a8b7a36724337df811f4d0a
PadminiRao <47210874+PadminiRao@users.noreply.github.com>
2019-01-31 15:56:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button, View, Text, Image } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Hello, How are You?</Text>
+        <Button
+          title="Next"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Image</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
